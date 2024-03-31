b0d78352a0c5c65d3258309db716634cdaefa155
Anjalivlog <139326744+Anjalivlog@users.noreply.github.com>
2024-03-31 15:39:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
-const arr1 = [];
+
 const divide = (arr, n) => {
   // Write your code here
-	for (let start = 0; start < arr.length; start++) {
-		let subarr = "";
-		let sum = 0;
-		for (let end = 0; end < arr.length; end++) {
-			sum = sum + arr[end];
-            subarr = subarr + arr[end] + "";
-			if (sum <= n) {
-				arr1.push(subarr);
-			}
-		}
-	}
-	return arr1;
+	let chunks = [];
+    let chunk = [];
+    let sum = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (sum + arr[i] > n) {
+            // start a new chunk
+            chunks.push(chunk);
+            chunk = [arr[i]];
+            sum = arr[i];
+        } else {
+            // add to the current chunk
+            chunk.push(arr[i]);
+            sum += arr[i];
+        }
+    }
+
+    // add the last chunk
+    if (chunk.length > 0) {
+        chunks.push(chunk);
+    }
+
+    return chunks;
 };
 
 const n = prompt("Enter n: ");
