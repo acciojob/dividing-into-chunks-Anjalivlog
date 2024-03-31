e891d3897096048c7b7b717c6e2cf6133e6ca238
Anjalivlog <139326744+Anjalivlog@users.noreply.github.com>
2024-03-31 15:03:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	fo
 };
 
 const n = prompt("Enter n: ");
-alert(JSON.stringify(divide(arr, n)));
+// alert(JSON.stringify(divide(arr, n)));
